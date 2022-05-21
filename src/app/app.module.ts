606560432375978805371c665848b43b9c5dc5e0
Anderson <andersonv@ymail.com>
2022-05-21 18:48:56 -0300

    com aba dívida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// // ---- localização - pt-BR -> utilizado para mostrar valor da dívida do aluno em Real
+// import { registerLocaleData } from '@angular/common';
+// import ptBR from '@angular/common/locales/pt';
+// registerLocaleData(ptBR);
+
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'pt'
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/lista_alunos.ts
@@ -0,0 +1,37 @@
+export const alunos = [
+  {
+    nome: "Anderson Viergutz",
+    ru: 3589884,
+    nome_curso: "BACHARELADO EM ENGENHARIA DE SOFTWARE",
+    aniversario: "15 de Dezembro",
+    debito: 0
+  },
+  {
+    nome: "Patrícia Mates",
+    ru: 3589885,
+    nome_curso: "CIÊNCIA DE DADOS",
+    aniversario: "02 de Dezembro",
+    debito: 389.80
+  },
+  {
+    nome: "Joao Souza",
+    ru: 3589886,
+    nome_curso: "CIÊNCIA DE DADOS",
+    aniversario: "22 de Março",
+    debito: 0
+  },
+  {
+    nome: "Marcelo de Lima",
+    ru: 3589887,
+    nome_curso: "BACHARELADO EM ENGENHARIA DE SOFTWARE",
+    aniversario: "11 de Janeiro",
+    debito: 389.80
+  },
+  {
+    nome: "Ana Maria Ribeiro",
+    ru: 3589888,
+    nome_curso: "BACHARELADO EM ENGENHARIA DE SOFTWARE",
+    aniversario: "30 de Agosto",
+    debito: 779.6
+  }
+];
